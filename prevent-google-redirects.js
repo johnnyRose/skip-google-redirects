165bba35a488177708f7bdb431be168cf4f2df13
John Rosewicz <john.rosewicz@gmail.com>
2019-09-25 16:51:51 -0500

    Some cleanup

--- prevent-google-redirects.js
+++ prevent-google-redirects.js
@@ -1,19 +1,3 @@
-//console.log("here we are");
-
-// try {
-	// browser.webnavigation.onbeforenavigate.addlistener(function (details) {
-		
-		// console.log(details.url);
-		
-		// return {
-			// redirecturl: "https://www.google.com"
-		// };
-		
-	// }, { url: [ { urlcontains: "google.com" } ] });
-// } catch (e) {
-	// console.log(e.message);
-// }
-	
 
 try {
 	browser.webRequest.onBeforeRequest.addListener(function (details) {
@@ -29,46 +13,13 @@ try {
 			
 			return {
 				redirectUrl: newUrl
-				//cancel: true
 			};
 		}
 
 	}, { urls: [
-			//"*://*.google.com/*"
-			"<all_urls>"
+			"*://*.google.com/*"
+			//"<all_urls>"
 			] }, ["blocking"]);
 } catch (e) {
 	console.log(e.message);
 }
-
-console.log("2");
-
-// var httpRequestObserver =
-// {
-  // observe: function(subject, topic, data)
-  // {
-	  // console.log("observing");
-	  
-    // if (topic == "http-on-modify-request") {
-      // var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
-      // httpChannel.setRequestHeader("X-Hello", "World", false);
-    // }
-  // },
-
-  // get observerService() {
-    // return Cc["@mozilla.org/observer-service;1"]
-                     // .getService(Ci.nsIObserverService);
-  // },
-
-  // register: function()
-  // {
-    // this.observerService.addObserver(this, "http-on-modify-request", false);
-  // },
-
-  // unregister: function()
-  // {
-    // this.observerService.removeObserver(this, "http-on-modify-request");
-  // }
-// };
-
-// httpRequestObserver.register();
